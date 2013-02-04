d2c9993b8ed761c96763b33845c0266ae12b8ff2
Anoj Perera <anojmperera@gmail.com>
2013-02-04 21:01:45 +0000

    Main executable file for thor

---
+++ src/thorexec.h
@@ -0,0 +1,16 @@
+/* Declaration of test executetables */
+/* 04/02/2013 */
+#ifndef __THOREXEC_H__
+#define __THOREXEC_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+int thorahuexec_main(int argc, char** argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __THOREXEC_H__ */
