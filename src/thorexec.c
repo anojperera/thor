3f880cd1f8d8e04b64d5b4eacdacce0681b565d6
Anoj Perera <anojmperera@gmail.com>
2013-02-17 18:34:15 +0000

    Main program options added.

--- src/thorexec.c
+++ src/thorexec.c
@@ -6,11 +6,12 @@
 #include <string.h>
 
 #include "thorexec.h"
-
+#define PROG_MSG "\nSelect Below Option: \nAHU Performance Test [1]\nDamper Close Test [2]\nOption [1/2]: "
 int main(int argc, char** argv)
 {
     int _next_opt;
     int _opt_st_flg = 0;
+    int _prog_id = 0;
     /* short options */
     const char* const _short_opts = "a:d:";
     const struct option _long_opts[] = {
@@ -26,20 +27,34 @@ int main(int argc, char** argv)
 	    switch(_next_opt)
 		{
 		case 'a':
-		    thorahuexec_main(argc, argv);
+		    _prog_id = 1;
 		    _opt_st_flg = 1;
 		    break;
 		case 'd':
-		    thoractstexec_main(argc, argv);
+		    _prog_id = 2;
 		    _opt_st_flg = 1;
 		    break;
 		case -1:
 		default:
+		    printf(PROG_MSG);
+		    scanf("%i", &_prog_id);
 		    break;
 		}
 	    if(_opt_st_flg)
 		break;
 	}while(_next_opt != -1);
-    
+
+    /* execute program */
+    switch(_prog_id)
+	{
+	case 1:
+	    thorahuexec_main(argc, argv);
+	    break;
+	case 2:
+	    thoractstexec_main(argc, argv);
+	    break;
+	default:
+	    fprintf(stderr, "\nInvalid Option\n");
+	}
     return 0;
 }
