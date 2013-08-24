6aa51e3a9b6daf29c71d23907daa6806fba202f1
Anoj Perera <anojmperera@gmail.com>
2013-08-24 22:06:06 +0100

    Header file refined.
    New class is defined to replace the existing sensor class.
    Uniform and defined sensor interface.

--- src/thgsensor.h
+++ src/thgsensor.h
@@ -3,8 +3,8 @@
  * derives equation of the straigt line between voltage
  * and physical property */
 
-#ifndef __THGSENS_H__
-#define __THGSENS_H__
+#ifndef _THGSENSOR_H_
+#define _THGSENSOR_H_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,31 +17,48 @@
 #include "thornifix.h"
 #include "thbuff.h"
 
-typedef struct _thgsens thgsens;
-typedef unsigned int (*gthsen_fptr)(void*, double*);	/* generic function pointer */
+#define THGS_MIN_VOLT 0.0
+#define THGS_MAX_VOLT 10.0
+#define THGS_CH_NAME_SZ 64
+#define THGS_CH_RBUFF_SZ 20
 
-struct _thgsens
+/* macros for calculating gradient and intercept */
+#define THGS_CALC_GRAD(sensor)						\
+    (sensor)->var_grad = (sensor)->var_max - (sensor)->var_min /	\
+	(THGS_MAX_VOLT - THGS_MIN_VOLT)
+
+#define THGS_CALC_INTR(sensor)						\
+    (sensor)->var_intc = (sensor)->var_min - (sensor)->var_grad *	\
+	THGS_MIN_VOLT
+
+typedef struct _thgsensor thgsensor;
+
+struct _thgsensor
 {
-    char* var_ch_name;				/* channel name */
-    TaskHandle* var_task;			/* pointer to task */
-    thbuff* var_buffval;			/* buffer value */
+    char var_ch_name[THGS_CH_NAME_SZ];		/* channel name */
+    double var_ave_buff[THGS_CH_RBUFF_SZ];	/* averaging buffer */
+    
+    TaskHandle* var_task;			/* pointer to task handle */
+
+    /* calibration buffers */
     const double* _var_cal_buff_x;		/* calibration buffer x */
     const double* _var_cal_buff_y;		/* calibration buffer y */
+    int _var_calbuff_sz;			/* calibration buffer size */    
+    
+    double var_grad;				/* gradient of straight line */
+    double var_intc;				/* intercept point */    
     double var_min;				/* minimum physical dimension */
     double var_max;				/* maximum physical dimension */
+    
     double var_val;				/* sensor return value */
     float64 var_raw;				/* raw voltage value from sensor */
+
     unsigned int var_termflg;			/* termination flag for data gathering */
-    unsigned int _var_raw_set;			/* flag to indicate raw value set */
-    int _var_calbuff_sz;			/* buffer size */
-    gthsen_fptr var_update;			/* update function pointer - if the function
-						 * pointer was assigned, it shall be called
-						 * is used for updating ui */
-    double var_grad;				/* gradient of straight line */
-    double var_intc;				/* intercept point */
+    unsigned int _var_raw_set;			/* flag to indicate raw value set */    
+    unsigned int var_flg;			/* flag to indicate enabled or disable state */    
     unsigned int var_okflg;			/* flag to indicate ranges has been set */
+    
     void* sobj_ptr;				/* pointer to external object */
-    unsigned int var_flg;			/* flag to indicate enabled or disable state */
 };
 
 #ifdef __cplusplus
@@ -49,14 +66,27 @@ extern "C" {
 #endif
 
     /* Constructor with function pointer and channel name */
-    int thgsens_new(thgsens** obj, const char* ch_name,
-		    TaskHandle* task, gthsen_fptr fptr, void* data);
+    int thgsensor_new(thgsensor* obj,		/* object pointer to initialise */
+		      const char* ch_name,	/* channel name */
+		      TaskHandle* task,		/* NI task handle */
+		      void* data);		/* optional external object pointer */
 
     /* destructor */
-    void thgsens_delete(thgsens** obj);
+    void thgsensor_delete(thgsensor* obj);
 
     /* set range */
-    extern inline int thgsens_set_range(thgsens* obj, double min, double max);
+    inline __attribute__ ((always_inline)) static int thgsensor_set_range(thgsensor* obj, double min, double max)
+    {
+	if(obj == NULL)
+	    return 1;
+	obj->var_min = min;
+	obj->var_max = max;
+
+	/* calculate gradient and intercept */
+	THGS_CALC_GRAD(obj);
+	THGS_CALC_INTR(obj);
+	return 0;
+    }
 
     /* Add new raw value */
     inline __attribute__ ((always_inline)) static int thgsens_add_value(thgsens* obj, float64 val)
@@ -95,4 +125,4 @@ extern "C" {
 }
 #endif
 
-#endif /* __THGSENS_H__ */
+#endif /* _THGSENSOR_H_ */
