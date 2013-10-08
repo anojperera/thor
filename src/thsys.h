91a2c949cd6c556e0cd67f2bff08402e379e2f42
Anoj Perera <anojmperera@gmail.com>
2013-10-08 16:29:00 +0100

    Return type of delete method changed to void pointer

--- src/thsys.h
+++ src/thsys.h
@@ -4,14 +4,19 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include "thornifix.h"
 
-#define THSYS_AI_CHANNELS "Dev1/ai0:10";
-#define THSYS_A0_CHANNELS "Dev1/ao0:1";
+#define THSYS_AI_CHANNELS "Dev1/ai0:10"
+#define THSYS_A0_CHANNELS "Dev1/ao0:1"
+#define THSYS_EMPTY_STR ""
+#define THSYS_CLOCK_SOURCE "OnboardClock"
 #define THSYS_NUM_AI_CHANNELS 11
 #define THSYS_NUM_AO_CHANNELS 2
 #define THSYS_MIN_VAL 0.0
 #define THSYS_MAX_VAL 10.0
+#define THSYS_DEFAULT_SAMPLE_RATE 8.0
+#define THSYS_READ_WRITE_FACTOR 1.5
 
 typedef struct _thsys thsys;
 
@@ -32,6 +37,8 @@ struct _thsys
     float64 var_sample_rate;		/* sample rate */
     float64 var_inbuff[THSYS_NUM_AI_CHANNELS];
     float64 var_outbuff[THSYS_NUM_AO_CHANNELS];
+
+    pthread_t var_thread;		/* thread id */
 };
 
 #ifdef __cplusplus
@@ -40,11 +47,15 @@ extern "C" {
 
     /* initialise system struct */
     int thsys_init(thsys* obj);
-    int thsys_delete(thsys* obj);
+    void thsys_delete(thsys* obj);
 
     /* start method */
     int thsys_start(thsys* obj);
     int thsys_stop(thsys* obj);
+
+    /* set sampling rate */
+#define thsys_set_sample_rate(obj, val)		\
+    (obj)->var_sample_rate = (val>0.0? val : THSYS_DEFAULT_SAMPLE_RATE)
     
 #ifdef __cplusplus
 }
