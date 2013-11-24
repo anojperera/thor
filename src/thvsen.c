2168f9c7d6dc6bb3cce57a463379a85941b71347
Anoj Perera <anojmperera@gmail.com>
2013-11-24 12:51:22 +0000

    Velocity sensors fully implemented.
    Calibration figures for the probes are not set.

--- src/thvsen.c
+++ src/thvsen.c
@@ -12,7 +12,7 @@
  * into internal networks.
  */
 static int _thvsen_read_config(thvsen* obj);
-
+static int _thvsen_read_array(const config_setting_t* setting, size_t sz, double** arr);
 
 /* Constructor */
 int thvsen_new(thvsen* obj, config_setting_t* setting, size_t num)
@@ -121,7 +121,10 @@ double thvsen_get_val(thvsen* obj)
 /* Read configuration settings */
 static int _thvsen_read_config(thvsen* obj)
 {
-    int i, _num;
+    int i, j, _num, _num2, _num3;
+    config_setting_t* _setting, *_t_setting, *_t2_setting;
+    const char* _name;
+    
     /* check if the configuration settings has been set */
     if(!obj->_var_setting)
 	return -1;
@@ -136,6 +139,79 @@ static int _thvsen_read_config(thvsen* obj)
      * Iterate over the settings collection and obtain the setting.
      * for each statndard type. Calibration arrays are.
      */
+    for(i=0; i<_num; i++)
+	{
+	    /* Get setting based on index */
+	    _setting = config_setting_get_elem(obj->_var_setting, i);
 
+	    /* Check for setting if null, continue with loop */
+	    if(!_setting)
+		continue;
 
+	    /* Get number of settings */
+	    _num2 = config_setting_length(_setting);
+	    for(j=0; j<_num2; j++)
+		{
+		    switch(j)
+			{
+			case 0:
+			    _name = config_setting_get_string_elem(_setting, j);
+			    strncpy(obj->_var_configs[i].var_sen_name_buff, _name, THVSEN_SEN_NAME_BUFF-1);
+			    obj->_var_configs[i].var_sen_name_buff[THVSEN_SEN_NAME_BUFF-1] = '\0';
+			    break;
+			case 1:
+			    obj->_var_configs[i].var_range_min = config_setting_get_float_elem(_setting, j);
+			    break;
+			case 2:
+			    obj->_var_configs[i].var_range_max = config_setting_get_float_elem(_setting, j);
+			    break;
+			case 3:
+			    /*
+			     * Load calibration settings if exists. Index 3 and 4 loads calibration
+			     * settings as defined in the configuration file.
+			     */
+			    _t2_setting = config_setting_get_member(_setting, THOR_CONFIG_CALX);
+			    if(_t2_setting == NULL)
+				break;
+			    
+			    _num3 = config_setting_length(_t2_setting);
+			    _thvsen_read_array(_t2_setting, _num3, &obj->_var_configs[i].var_calib_x);
+			    break;
+			case 4:
+			    _t2_setting = config_setting_get_member(_setting, THOR_CONFIG_CALY);
+			    if(_t2_setting == NULL)
+				break;			    
+			    _thvsen_read_array(_t2_setting, _num3, &obj->_var_configs[i].var_calib_y);
+			    break;
+			default:
+			};
+		}
+	    
+	}
+
+}
+
+/*
+ * Loads array elements defined in a configuration file to the pointer pointed by arr.
+ * The size of the array must be greater than zeor. A new arr of size sz shall be allocated
+ */
+static int _thvsen_read_array(const config_setting_t* setting, size_t sz, double** arr)
+{
+    int i;
+
+    /* check for arguments */
+    if(setting == NULL || sz <= 0 || arr == NULL)
+	return -1;
+
+    /* Allocate memory to the buffer */
+    (*arr) = (double*) calloc(sz, sizeof(double));
+
+    /*
+     * Iterate through the settings collection and set the values
+     * defined in the configuration file to the buffer elements
+     */
+    for(i=0; i<sz; i++)
+	(*arr)[i] = config_setting_get_float_elem(setting, i);
+
+    return 0;
 }
