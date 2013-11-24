d8dab61f2147b704393a8400db9ffeca2b3fe9a7
Anoj Perera <anojmperera@gmail.com>
2013-11-24 15:37:31 +0000

    Implementation of thsen completed.

---
+++ src/thsen.c
@@ -0,0 +1,36 @@
+#include "thsen.h"
+
+
+int thsen_init(thsen* obj)
+{
+    it(obj == NULL)
+	return -1;
+
+    obj->var_setting = NULL;
+    obj->var_child = NULL;
+    
+    obj->_var_num_config = 0;
+    obj->_var_configs = NULL;
+
+    obj->var_thsen_del_fptr = NULL;
+    obj->var_thsen_get_fptr = NULL;
+    obj->var_thsen_set_config_fptr = NULL;
+
+    obj->var_init_flg = 1;
+    return 0;
+}
+
+void thsen_delete(thsen* obj)
+{
+    if(obj == NULL)
+	return;
+
+    if(obj->_var_num_config)
+	free(obj->_var_configs);
+    obj->_var_configs = NULL;
+
+    if(obj->var_thsen_del_fptr)
+	obj->var_thsen_del_fptr(obj->var_child);
+
+    return 0;
+}
