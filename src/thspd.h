1d4e6a9c9ec0ffe3d778d833ae0217d4c0100919
Anoj Perera <anojmperera@gmail.com>
2013-11-25 15:11:30 +0000

    Header file added for declaring interfaces for the speed sensor class.

---
+++ src/thspd.h
@@ -0,0 +1,16 @@
+/*
+ * Speed sensor class is an abstract of generic sensor class.
+ */
+#ifndef __THSPD_H__
+#define __THSPD_H__
+
+#include "thgsensor.h"
+
+/* Redefined interfaces */
+#define thgspd thgsensor
+#define thspd_new thgsensor_new
+#define thspd_delete thgsensor_delete
+
+
+#endif /* __THSPD_H__ */
+
