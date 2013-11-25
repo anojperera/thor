df4fa4fe1eb72bc2095e36b3822525c77b715568
Anoj Perera <anojmperera@gmail.com>
2013-11-25 15:13:33 +0000

    Header file added for temperature class.

---
+++ src/thtmp.h
@@ -0,0 +1,16 @@
+/*
+ * Header file for describing the interfaces for the temperature class.
+ * Inherits generic sensor class.
+ */
+#ifndef __THTMP_H__
+#define __THTMP_H__
+
+#include "thgsensor.h"
+
+#define thtmp thgsensor
+
+#define thtmp_new thgsensor_new
+#define thtmp_delete thgsensor_delete
+
+
+#endif /* __THTMP_H__ */
