4564d07b2d7de49f26d9ff6c6b6f9b4f74596ad2
Anoj Perera <anojmperera@gmail.com>
2013-12-02 09:30:03 +0000

    Test script svr test program added.

--- src/make_test.sh
+++ src/make_test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 gcc -g -Wall -O0 -o test main.c thsys.c -I/usr/local/natinst/nidaqmxbase/include/ /usr/local/natinst/nidaqmxbase/lib/libnidaqmxbase.so.3.7.0 -lm -lpthread
+gcc -g -Wall -O0 -o thsvr thsvre.c thsys.c thcon.c -I/usr/local/natinst/nidaqmxbase/include/ /usr/local/natinst/nidaqmxbase/lib/libnidaqmxbase.so.3.7.0 -lm -lpthread
+
