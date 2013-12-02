a9172f79defb19c198e94229d8b333900c50a653
Anoj Perera <anojmperera@gmail.com>
2013-12-02 09:37:19 +0000

    Test script updated.

--- src/make_test.sh
+++ src/make_test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -g -Wall -O0 -o test main.c thsys.c -I/usr/local/natinst/nidaqmxbase/include/ /usr/local/natinst/nidaqmxbase/lib/libnidaqmxbase.so.3.7.0 -lm -lpthread
-gcc -g -Wall -O0 -o thsvr thsvre.c thsys.c thcon.c -I/usr/local/natinst/nidaqmxbase/include/ /usr/local/natinst/nidaqmxbase/lib/libnidaqmxbase.so.3.7.0 -lm -lpthread
+gcc -g -Wall -O0 -o thsvr thsvre.c thsys.c thcon.c -I/usr/local/natinst/nidaqmxbase/include/ -I/usr/include/libxml2/ /usr/local/natinst/nidaqmxbase/lib/libnidaqmxbase.so.3.7.0 -lm -lxml2 -lconfig -lpthread
 
