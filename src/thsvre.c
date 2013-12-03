b60ee071b79f2632ac91c5996552705dbb1e33d6
Anoj Perera <anojmperera@gmail.com>
2013-12-03 09:41:45 +0000

    Signal handler aadded a variable to exit from main loop.

--- src/thsvre.c
+++ src/thsvre.c
@@ -10,7 +10,8 @@
 #define THSVR_DEFAULT_CONFIG_PATH1 "thor.cfg"
 #define THSVR_DEFAULT_CONFIG_PATH2 "../config/thor.cfg"
 #define THSVR_DEFAULT_CONFIG_PATH3 "/etc/thor.cfg"
-    
+
+volatile sig_atomic_t _flg = 1;
 thsvr _var_svr;							/* Server component */
 config_t _var_config;						/* Configuration settings object */
 
@@ -35,7 +36,7 @@ int main(int argc, char** argv)
 	goto thsvre_exit;
 
     /* Attach a signal handler */
-    signal(SIGTERM, _thsvre_sigterm_handler);
+    signal(SIGKILL, _thsvre_sigterm_handler);
     signal(SIGINT, _thsvre_sigterm_handler);    
     
     /* Start the program */
@@ -45,7 +46,7 @@ int main(int argc, char** argv)
      * This is the main loop. run for ever
      * until terminated.
      */
-    while(1)
+    while(_flg)
 	sleep(1);
 
  thsvre_exit:
@@ -79,17 +80,18 @@ static int _thsvre_load_config(void)
     return -1;
 }
 
+
 /* Signal handler */
 static void _thsvre_sigterm_handler(int signo)
 {
-    if(signo == SIGINT)
+    if(signo == SIGINT || signo == SIGKILL)
 	{
 	    /* Stop the server */
 	    thsvr_stop(&_var_svr);
 	    thsvr_delete(&_var_svr);
-	    
-	    /* Destroy the configuration object */
-	    config_destroy(&_var_config);
+
+	    /* Set flag to exit main loop */
+	    _flg = 0;
 	}
 
     return;
