6826cea3a600aaeb06b07b5f28209b4487ed4ab6
Anoj Perera <anojmperera@gmail.com>
2014-02-03 23:49:24 +0000

    Initialisation of the curses system and clean up implemented in the executable.

--- src/thappe.c
+++ src/thappe.c
@@ -3,16 +3,24 @@
 #include <stdlib.h>
 #include "thapp.h"
 #include "thapp_ahu.h"
+#include "thapp_lkg.h"
 
 int main(int argc, char** argv)
 {
     thapp* _ahu;
-
+    
+    /*
+     * Initialise ncurses system.
+     */
+    initscr();
+    
     _ahu = thapp_ahu_new();
 
     thapp_start(_ahu);
     
     thapp_ahu_delete(THOR_AHU(_ahu));
+
+    endwin();
     return 0;
 }
 
