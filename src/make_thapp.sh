a0efc2a890e84a694e2eafcf5b2e2569e5f1e9d4
Anoj Perera <anojmperera@gmail.com>
2014-02-04 00:37:20 +0000

    Make script added with the leakage class.

--- src/make_thapp.sh
+++ src/make_thapp.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-gcc -g -Wall -O0 -o thahu thappe.c thapp_ahu.c thapp.c thcon.c thsen.c thgsensor.c thvprb.c thvsen.c thsmsen.c thornifix.c -I/usr/include/libxml2/ -lalist -lxml2 -lcurl -lconfig -lm -lalist -lncurses -lpthread
+gcc -g -Wall -O0 -o thor thappe.c thapp_ahu.c thapp_lkg.c thapp.c thcon.c thsen.c thgsensor.c thvprb.c thvsen.c thsmsen.c thornifix.c \
+-I/usr/include/libxml2/ -lalist -lxml2 -lcurl -lconfig -lm -lalist -lmenu -lncurses -lpthread
