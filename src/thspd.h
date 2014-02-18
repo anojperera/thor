546aa8dc6400d965480bee510c7f7e75ccc0137b
Anoj Perera <anojmperera@gmail.com>
2014-02-18 00:35:58 +0000

    Speed sensor class defined.

--- src/thspd.h
+++ src/thspd.h
@@ -4,13 +4,35 @@
 #ifndef __THSPD_H__
 #define __THSPD_H__
 
+#include "thsen.h"
 #include "thgsensor.h"
 
-/* Redefined interfaces */
-#define thgspd thgsensor
-#define thspd_new thgsensor_new
-#define thspd_delete thgsensor_delete
+typedef struct _thspd thspd;
 
+struct _thspd
+{
+    thgsensor var_parent;
+    unsigned int var_int_flg;
+    unsigned int var_init_flg;
+
+    double var_val;
+
+    struct thsen_vftpr var_fptr;
+    void* var_child;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /* Constructor and destructor */
+    thsen* thspd_new(thspd* obj);
+    void thspd_delete(thspd* obj);
+
+
+#endif __cplusplus
+}
+#endif    
 
 #endif /* __THSPD_H__ */
 
