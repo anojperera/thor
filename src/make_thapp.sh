575016191fd77d70d2628e01091d7a8d24e78f85
Anoj Perera <anojmperera@gmail.com>
2014-02-18 01:01:17 +0000

    Make script updated.

--- src/make_thapp.sh
+++ src/make_thapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc -g -Wall -O0 -o thor thappe.c thapp_ahu.c thapp_lkg.c thapp.c thcon.c thsen.c thgsensor.c thvprb.c thvsen.c thsmsen.c thornifix.c \
+gcc -g -Wall -O0 -o thor thappe.c thapp_ahu.c thapp_lkg.c thapp.c thcon.c thsen.c thgsensor.c thvprb.c thvsen.c thsmsen.c thspd.c thornifix.c \
 -I/usr/include/libxml2/ -lalist -lxml2 -lcurl -lconfig -lm -lalist -lmenu -lncurses -lpthread
