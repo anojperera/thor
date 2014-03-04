ffc0a1dc9f352962eddc641b9e93d0a6babf8fc0
Anoj Perera <anojmperera@gmail.com>
2014-03-04 00:04:11 +0000

    Script amended to add daemon

--- src/make_asgard.sh
+++ src/make_asgard.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 g++ -g -Wall -O0 -o asgard thasgard.cc thasg_websock.cc thcon.c -I/usr/include/libxml2 -lstdc++ -lpthread -lxml2 -lconfig -lcurl -lwebsockets -lalist
+#
+#
+# Make daemon
+gcc -g -Wall -O0 -o asgard_svr thasgarde.c
