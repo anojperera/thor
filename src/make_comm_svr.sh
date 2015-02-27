87c61a3ef2f4421aa3dd13f239063e2bc97d1e00
Anoj Perera <anojmperera@gmail.com>
2015-02-27 21:24:09 +0000

    Bash script added for making a connection lib

---
+++ src/make_comm_svr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Creates a shared object of which expose a communication server
+gcc -g -Wall -O0 -c -fPIC thcon.c thornifix.c -I/usr/include/libxml2/
+mv *.o ../bin/
+gcc -shared -Wl,-soname,libcomm.so.1 -o ../bin/libcomm.so.1.0.1 ../bin/*.o
+rm ../bin/*.o
+exit 0
