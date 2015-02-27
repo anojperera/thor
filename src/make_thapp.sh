2ea22d5070a043b1e709036d53383988f201c089
Anoj Perera <anojmperera@gmail.com>
2015-02-27 22:10:31 +0000

    Made to bin folder

--- src/make_thapp.sh
+++ src/make_thapp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Application program
-gcc -g -Wall -O0 -o thor -DTHOR_INC_NI thappe.c thapp_ahu.c thapp_lkg.c thapp.c thcon.c \
+gcc -g -Wall -O0 -o ../bin/thor -DTHOR_INC_NI thappe.c thapp_ahu.c thapp_lkg.c thapp.c thcon.c \
 	thsen.c thgsensor.c thvprb.c thvsen.c thsmsen.c thspd.c thornifix.c \
 	-I/usr/include/libxml2/ -lalist -lxml2 -lcurl -lconfig -lm -lalist -lmenu -lncurses -lpthread
 
