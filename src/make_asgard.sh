7834d61926446e7496c64dc3d48b16e751cd17c4
Anoj Perera <anojmperera@gmail.com>
2015-02-27 23:44:46 +0000

    Bash make script updated

--- src/make_asgard.sh
+++ src/make_asgard.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #
 g++ -g -Wall -O0 -o asgard thasgard.cc thasg_websock.cc thcon.c \
-	-I/usr/include/libxml2 -lstdc++ -lpthread -lxml2 -lconfig -lcurl -lwebsockets -lalist
+	-I/home/pyrus/Prog/C++/libwebsockets/lib/ -I/usr/include/libxml2/ -lstdc++ -lpthread -lxml2 -lz -lm -lssl -lcrypto\
+	-L/usr/lib/x86_64-linux-gnu/imlib2/loaders/ -lconfig -lcurl \
+	/home/pyrus/Prog/C++/libwebsockets/lib/lib/libwebsockets.a -lalist
 #
 #
 # Make daemon
