82f0f392e85e9ec9ae1d59bcef10520f82045007
Anoj Perera <anojmperera@gmail.com>
2015-03-23 22:00:43 +0000

    Make bash script updated.

--- src/make_comm_svr.sh
+++ src/make_comm_svr.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# Get directory paths
+cwd=`pwd`
+parent=$(dirname $cwd )
+include_folder="/inc/"
+include_path="$parent$include_folder"
+
 # Creates a shared object of which expose a communication server
-gcc -g -Wall -O0 -c -fPIC thcon.c thornifix.c -I/home/pyrus/Prog/C++/thor/inc/ -I/usr/include/libxml2/
+gcc -g -Wall -O0 -c -fPIC thcon.c thornifix.c -I$include_path -I/usr/include/libxml2/
 mv *.o ../bin/
 gcc -shared -Wl,-soname,libcomm.so.1 -o ../bin/libcomm.so.1.0.1 ../bin/*.o
+rm ../bin/*.o
+# Create static archive
+gcc -g -Wall -O0 -c thcon.c thornifix.c -I$include_path -I/usr/include/libxml2/
+mv *.o ../bin/
+ar rcs ../bin/libcomm.a ../bin/*.o
 rm ../bin/*.o
 exit 0
